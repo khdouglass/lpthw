5addc30ec030313fff42edf697ae48b3c9cc9d6a
Kathryn Douglass <kd@Kathryns-MacBook-Air.local>
2016-11-02 18:15:07 -0700

    first commit

---
+++ ex11.py
@@ -0,0 +1,21 @@
+#print "How old are you?",
+#age = raw_input()
+#print "How tall are you?", 
+#height = raw_input()
+#print "How much do you weigh?",
+#weight = raw_input()
+
+#print "So, you're %r old, %r tall and %r heavy." % (
+	#age, height, weight)
+
+print "How old you are?"
+age = int(raw_input())
+
+print "Hold old is your mom?"
+mom_age = int(raw_input())
+
+difference = mom_age - age
+
+print "So your mom is %d years older than you." % (
+	difference)
+
