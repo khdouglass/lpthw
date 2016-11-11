df14706c6a5313763266a2069d9e45be2ddceea1
khdouglass <khdouglass@gmail.com>
2016-11-10 16:53:39 -0800

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second = argv
+
+print "This script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "your third variable is:", raw_input()
